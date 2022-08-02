2c0089bf850b9348101a0ee49cfa044c551b25f7
Mahonri Maia <mahonri.maia@gmail.com>
2022-08-02 17:39:34 -0300

    Atualização de usuário com PUT

--- src/main/java/com/example/mongodb/domain/User.java
+++ src/main/java/com/example/mongodb/domain/User.java
@@ -24,6 +24,14 @@ public class User implements Serializable {
         this.email = email;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
@@ -38,10 +46,6 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getId() {
-        return id;
     }
 
     @Override
